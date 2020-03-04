fcd9a61d2347c0bf53f6cfb1a58b3edfd9ca384d
youngikjung <youngikjung@winsure.co.kr>
2020-03-03 22:30:57 -0800

    프로젝트 생성

---
+++ src/test/java/com/youngik/springwebservice/webserviceTests.java
@@ -0,0 +1,13 @@
+package com.youngik.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class webserviceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
